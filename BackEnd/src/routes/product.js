c5cad8f205ebd8b9e9fb7ff50cf751a0a2fbe72c
Hungttph13998 <hungttph13998@fpt.edu.vn>
2022-07-27 20:32:05 +0700

    #1 create router product

---
+++ BackEnd/src/routes/product.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { list, read, create, remove, update } from "../controllers/product.js";
+
+const router = express.Router();
+
+router.get("/products", list);
+router.get("/product", read);
+router.post("product", create);
+router.delete("/product/:id", remove);
+router.patch("/product/:id", update);
+
+export default router;
