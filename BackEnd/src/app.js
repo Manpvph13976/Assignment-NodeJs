7e65a7b4e021c62c773a50b7bcaabe3b4bb0630a
Manpvph13976 <manpvph13976@fpt.edu.vn>
2022-07-27 21:15:22 +0700

    #33 fix bug router-category

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -2,10 +2,12 @@ import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
 import routerProduct from "./routes/product";
+import routerCategory from "./routes/category"
 
 const app = express();
 app.use(express.json());
 app.use("/api", routerProduct);
+app.use("/api", routerCategory);
 
 mongoose.connect("mongodb://localhost:27017/nhom3", () => {
   console.log("successfully");
