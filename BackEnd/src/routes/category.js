8a5d81b057dd51dd0b6c48c4bb178730af76c00a
Manpvph13976 <manpvph13976@fpt.edu.vn>
2022-07-27 21:19:54 +0700

    #2 fix bug model-product

--- BackEnd/src/routes/category.js
+++ BackEnd/src/routes/category.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { list, read, create, remove, update } from "../controllers/category.js";
+import { list, read, add, remove, update } from "../controllers/category.js";
 
 const router = express.Router();
 
 router.get("/category", list);
 router.get("/category", read);
-router.post("category", create);
+router.post("category", add);
 router.delete("/category/:id", remove);
 router.patch("/category/:id", update);
 
