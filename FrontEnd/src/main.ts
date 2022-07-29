cead999591ed1fe15514699875580960c1442198
ThanhBinh0508 <101467063+ThanhBinh0508@users.noreply.github.com>
2022-07-29 15:44:20 +0700

    #15 create detail client

--- FrontEnd/src/main.ts
+++ FrontEnd/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import Navigo from 'navigo'
 import HomePage from './pages/home'
+import Detail from './pages/client/detail'
 const router = new Navigo('/', {linksSelector: "a"})
 
 const print = async (component)=>{
@@ -9,6 +10,9 @@ const print = async (component)=>{
 router.on({
     "/":()=>{
         print(HomePage)
+    },
+    "/detail":()=>{
+        print(Detail)
     }
 })
 router.resolve()
