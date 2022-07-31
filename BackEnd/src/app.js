97a28f3536f1ded89a49c95221d46c238a5a23df
Hungttph13998 <hungttph13998@fpt.edu.vn>
2022-07-31 17:31:31 +0700

    #54 fix bug router auth

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -2,14 +2,14 @@ import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
 import routerProduct from "./routes/product";
-import routerCategory from "./routes/category"
-import routerAuth from "./routes/auth"
+import routerCategory from "./routes/category";
+import routerAuth from "./routes/user";
 
 const app = express();
 app.use(express.json());
 app.use("/api", routerProduct);
 app.use("/api", routerCategory);
-app.use("/api", routerAuth)
+app.use("/api", routerAuth);
 
 mongoose.connect("mongodb://localhost:27017/nhom3", () => {
   console.log("successfully");
